e92df29101c8f5034880a44463e672a4c3ff5638
Xiao Tan <xiaotan828@gmail.com>
2018-06-05 22:31:46 +0800

    Site updated: 2018-06-05 22:31:46

--- js/src/mainiframe.js
+++
@@ -1,7 +0,0 @@
-
-function changeFrameHeight(){
-        var ifm= document.getElementById("mainiframe");
-        ifm.height=document.documentElement.clientHeight-56;
-    }
-    window.onresize=function(){ changeFrameHeight();}
-    $(function(){changeFrameHeight();});
