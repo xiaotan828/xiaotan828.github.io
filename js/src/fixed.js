dad01dea059dd2a6adcbf1d77d3b672f504d06bb
xiaotan828 <xiaotan828@gmail.com>
2018-07-21 09:58:11 +0800

    Site updated: 2018-07-21 09:58:04

--- js/src/fixed.js
+++ js/src/fixed.js
@@ -1,26 +1,16 @@
-Window.onload=function (){
 
-    var $body = document.body,
-    //获取导航元素 id
-        $fixnav = document.getElementById("fixnav");
-        window.addEventListener('scroll', function() {
-        if($fixnav){
-            var top = $fixnav.offsetTop;
-            var width = $fixnav.offsetWidth;
-            if(getScrollTop() <= top){
-                $fixnav.style = "";
-            } else {
-                $fixnav.style.position = "fixed";//固定
-                $fixnav.style.top = "0";//与页面顶端无缝相接
-                $fixnav.style.width = "100%";//导航宽度
-                $fixnav.style.zIndex = "1";//悬浮上一层
-                $fixnav.style.opacity = "0.9";//透明度
-            }
++function ($) {
+	$(function() {
+    var header = $(".site-brand-wrapper");
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 200) {
+            header.removeClass('site-brand-wrapper').addClass("site-brand-wrapper-alt");
+        } else {
+            header.removeClass("site-brand-wrapper-alt").addClass('site-brand-wrapper');
         }
-        clearTimeout(timer);
-        timer = setTimeout(function fn() {
-            scrollCallback();
-        }, 200);
-    });	
-};
+    });
+});
 
+}(jQuery);
